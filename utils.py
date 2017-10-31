8a22b1d93a26e81365da6f5b95a498a939271c00
Sai Pavan D <saipvn4@gmail.com>
2017-11-01 00:35:46 +0530

    Added support for multi cities

--- utils.py
+++ utils.py
@@ -8,9 +8,17 @@ from subprocess import Popen, PIPE
 
 import os
 import threading
+import sys
+import collections
 
 # runs cmd and returns the output
 def run_cmd(cmd):
-    p = Popen(cmd, shell=True, stdout=PIPE )
+    p = Popen(cmd, shell=True, stdout=PIPE)
     out, err = p.communicate()
     return out
+
+# returns the list of cities
+def read_cities(inp):
+    with open(inp) as f:
+        content = f.readlines()
+    return [x.strip() for x in content]
